94cae26a3edee76feb427d8598647f7da08a1a8a
Jonathan130900 <jlb130900@gmail.com>
2025-02-21 10:45:54 +0100

    Add project files.

---
+++ Progetto_Settimanale_MVC_Scarpe/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Progetto_Settimanale_MVC_Scarpe.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
