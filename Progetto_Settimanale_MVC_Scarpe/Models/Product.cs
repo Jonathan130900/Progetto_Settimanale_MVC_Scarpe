774a3d60c736c14abf853604ade58e4d3ff6931a
Jonathan130900 <jlb130900@gmail.com>
2025-02-21 11:18:21 +0100

    Aggiunto Product in Models

---
+++ Progetto_Settimanale_MVC_Scarpe/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Progetto_Settimanale_MVC_Scarpe.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CoverImage { get; set; }
+        public string AdditionalImage1 { get; set; }
+        public string AdditionalImage2 { get; set; }
+    }
+}
