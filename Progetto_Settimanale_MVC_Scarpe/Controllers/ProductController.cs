00b20b13da4a946986e52cff55fa282cd057f211
Jonathan130900 <jlb130900@gmail.com>
2025-02-21 13:38:27 +0100

    Progetto Completato

---
+++ Progetto_Settimanale_MVC_Scarpe/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Progetto_Settimanale_MVC_Scarpe.Models;
+
+namespace Progetto_Settimanale_MVC_Scarpe.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = ProductRepository.Products;
+            return View(products);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var product = ProductRepository.Products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+                return NotFound();
+            return View(product);
+        }
+    }
+}
